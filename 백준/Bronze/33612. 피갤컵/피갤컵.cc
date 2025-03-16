cd1b3a65b9697dd35314225212ccbfd05fe597e7
최연우 <jjangguzi@naver.com>
2025-03-17 08:59:29 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <tuple>
+#include <numeric>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define LINF 9223372036854775807
+#define MOD 1000000007
+#define ll long long
+#define int long long
+#define pi pair<int, int>
+#define ti tuple<int, int, int>
+using namespace std;
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    int n;
+    cin >> n;
+    if (n == 1) {
+        cout << "2024 8\n";
+    }
+    else if (n == 2) {
+        cout << "2025 3\n";
+    }
+    else if (n == 3) {
+        cout << "2025 10\n";
+    }
+    else if (n == 4) {
+        cout << "2026 5\n";
+    }
+    else if (n == 5) {
+        cout << "2026 12\n";
+    }
+}
