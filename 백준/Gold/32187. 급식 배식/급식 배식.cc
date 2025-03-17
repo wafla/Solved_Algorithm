f3be69abca40ba641c155b93e32c8d4e8599f85a
최연우 <jjangguzi@naver.com>
2025-03-17 23:23:42 +0900

    [Gold V] Title: 급식 배식, Time: 52 ms, Memory: 7540 KB -BaekjoonHub

--- 백준/Gold/32187. 급식 배식/급식 배식.cc
+++ 백준/Gold/32187. 급식 배식/급식 배식.cc
@@ -28,32 +28,41 @@ signed main()
     cin.tie(NULL), cout.tie(NULL);
     int n, m;
     cin >> n >> m;
-    vector<vector<pi>> dp(n + 1, vector<pi>(1, { 0,0 }));
-    int ans = 0;
+    vector<vector<pi>> v(n + 1);
     for (int i = 1; i <= m; i++) {
         int x;
         cin >> x;
         for (int j = 0; j < x; j++) {
             int a, b;
             cin >> a >> b;
-            if (dp[a].size() == 1) {
-                dp[a].emplace_back(i, b);
+            v[a].emplace_back(i, b);
+        }
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        unordered_map<int, pi> dp;
+        for (int j = 0; j < v[i].size(); j++) {
+            int idx = v[i][j].X;
+            int num = v[i][j].Y;
+            if (j == 0) {
+                dp[idx].X = 0; // 항상 배식하지 않음
+                dp[idx].Y = num; // 항상 배식함
             }
             else {
-                int prev_idx = dp[a][dp[a].size() - 1].X;
-                if (prev_idx + 1 == i) {
-                    int tmp = max(dp[a][dp[a].size() - 1].Y, dp[a][dp[a].size() - 2].Y + b);
-                    dp[a].emplace_back(i, tmp);
+                int prev_idx = v[i][j - 1].X;
+                dp[idx].X = max(dp[prev_idx].X, dp[prev_idx].Y);
+                if (prev_idx + 1 == idx) {
+                    dp[idx].Y = dp[prev_idx].X + num;
                 }
                 else {
-                    dp[a].emplace_back(i, dp[a][dp[a].size() - 1].Y + b);
+                    dp[idx].Y = max(dp[prev_idx].X, dp[prev_idx].Y) + num;
                 }
             }
         }
-        if (i == m) {
-            for (int j = 1; j <= n; j++) {
-                ans += dp[j].back().Y;
-            }
+        if (v[i].size()) {
+            int last_idx = v[i][v[i].size() - 1].X;
+            ans += max(dp[last_idx].X, dp[last_idx].Y);
         }
     }
     cout << ans << '\n';
