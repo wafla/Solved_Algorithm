51960f5effa80510b27c1ac4385f2aa15df3e3c7
최연우 <jjangguzi@naver.com>
2025-06-05 14:22:04 +0900

    [Silver V] Title: 중복된 숫자, Time: 952 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <tuple>
+#include <numeric>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define LINF 9223372036854775807
+#define MOD 1000000007
+#define ll long long
+#define int long long
+#define pi pair<int, int>
+#define ti tuple<int, int, int>
+using namespace std;
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    
+    int a = n * (n - 1) / 2;
+    int b = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        b += x;
+    }
+    cout << b - a << '\n';
+}
