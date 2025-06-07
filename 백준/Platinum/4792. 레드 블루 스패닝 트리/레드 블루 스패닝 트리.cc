7e2946d0b4f379e7a5ba8583c46283bc810128bd
최연우 <jjangguzi@naver.com>
2025-06-07 15:44:50 +0900

    [Platinum III] Title: 레드 블루 스패닝 트리, Time: 372 ms, Memory: 34920 KB -BaekjoonHub

---
+++ 백준/Platinum/4792. 레드 블루 스패닝 트리/레드 블루 스패닝 트리.cc
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <tuple>
+#include <numeric>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define LINF 9223372036854775807
+#define MOD 998244353
+#define ll long long
+#define int long long
+#define pi pair<int, int>
+#define ti tuple<int, int, int>
+#define T pair<int, pi>
+using namespace std;
+int arr[1001] = { 0 };
+int find(int x) {
+	if (x == arr[x])
+		return x;
+	return arr[x] = find(arr[x]);
+}
+void merge(int a, int b) {
+	a = find(a), b = find(b);
+	if (a < b)
+		swap(a, b);
+	arr[a] = b;
+}
+signed main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	
+	while (1) {
+		int n, m, k;
+		cin >> n >> m >> k;
+		if (n == 0 && m == 0 && k == 0)
+			break;
+
+		priority_queue<T, vector<T>, greater<T>> PQ;
+		priority_queue<T, vector<T>> RPQ;
+		for (int i = 0; i < m; i++) {
+			char c;
+			int f, t;
+			cin >> c >> f >> t;
+			if (c == 'B') {
+				PQ.push({ 0,{f,t} });
+				RPQ.push({ 0,{f,t} });
+			}
+			else {
+				PQ.push({ 1,{f,t} });
+				RPQ.push({ 1,{f,t} });
+			}
+		}
+
+		int cnt = 0;
+		iota(arr + 1, arr + n + 1, 1);
+		int b_max = 0;
+		while (!PQ.empty()) {
+			auto cur = PQ.top();
+			PQ.pop();
+			int c = cur.X;
+			int a = cur.Y.X;
+			int b = cur.Y.Y;
+			if (find(a) == find(b)) {
+				continue;
+			}
+			else {
+				merge(a, b);
+				if (c == 0)
+					b_max++;
+				cnt++;
+			}
+			if (cnt == n - 1)
+				break;
+		}
+
+		cnt = 0;
+		iota(arr + 1, arr + n + 1, 1);
+		int b_min = 0;
+		while (!RPQ.empty()) {
+			auto cur = RPQ.top();
+			RPQ.pop();
+			int c = cur.X;
+			int a = cur.Y.X;
+			int b = cur.Y.Y;
+			if (find(a) == find(b)) {
+				continue;
+			}
+			else {
+				merge(a, b);
+				if (c == 0)
+					b_min++;
+				cnt++;
+			}
+			if (cnt == n - 1)
+				break;
+		}
+
+		if (b_min <= k && k <= b_max)
+			cout << 1 << '\n';
+		else
+			cout << 0 << '\n';
+	}
+}
