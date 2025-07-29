914ec8508b8d30d62665c7c55b403ae4fb72c136
최연우 <jjangguzi@naver.com>
2025-07-29 17:45:14 +0900

    [Bronze IV] Title: DORO, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <tuple>
+#include <numeric>
+#define X first
+#define Y second
+#define INF 0x3f3f3f3f
+#define LINF 9223372036854775807
+#define MOD 998244353
+#define ll long long
+#define int long long
+#define pi pair<int, int>
+#define ti tuple<int, int, int>
+#define T pair<int, pi>
+using namespace std;
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		cout << s << "DORO ";
+	}
+}
