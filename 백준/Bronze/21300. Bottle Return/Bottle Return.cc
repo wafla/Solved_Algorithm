32bc4e710cb415212696425ec54d2e480d8f12cb
최연우 <jjangguzi@naver.com>
2025-08-20 03:52:23 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <tuple>
+#include <numeric>
+#include <bit>
+#include <functional>
+#define X first
+#define Y second
+#define INF 1e18
+#define LINF 9223372036854775807
+#define MOD 1000000007
+#define ll long long
+#define int long long
+#define pi pair<int, int>
+#define ti tuple<int, int, int>
+#define T pair<int, pi>
+using namespace std;
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << (a + b + c + d + e + f) * 5 << '\n';
+}
