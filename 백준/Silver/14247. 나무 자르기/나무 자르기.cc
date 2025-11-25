c83b5e5a725aff5c503b09ea9dc7f513e6a09705
최연우 <jjangguzi@naver.com>
2025-11-26 02:36:27 +0900

    [Silver II] Title: 나무 자르기, Time: 16 ms, Memory: 3908 KB -BaekjoonHub

---
+++ 백준/Silver/14247. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <tuple>
+#include <numeric>
+#include <bit>
+#include <functional>
+#include <utility>
+#define X first
+#define Y second
+#define INF 1e18
+#define LINF 9223372036854775807
+#define MOD 1000000007
+#define ll long long
+#define int long long
+#define pi pair<int, int>
+#define ti tuple<int, int, int>
+#define T pair<int, pi>
+#define pdi pair<double, int>
+using namespace std;
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n, ans = 0;
+    cin >> n;
+
+    vector<int> h(n), a(n);
+    for (auto& i : h) {
+        cin >> i;
+        ans += i;
+    }
+
+    priority_queue<int, vector<int>> PQ;
+    for (auto& i : a) {
+        cin >> i;
+        PQ.push(i);
+    }
+
+    while (!PQ.empty()) {
+        auto cur = PQ.top();
+        PQ.pop();
+        ans += cur * --n;
+    }
+    cout << ans << '\n';
+}
