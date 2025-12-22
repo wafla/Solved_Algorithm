def28513207c68a121bdf05c9d049b29b149912c
최연우 <jjangguzi@naver.com>
2025-12-22 13:45:18 +0900

    [Bronze IV] Title: 신규 학과, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/34934. 신규 학과/신규 학과.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <tuple>
+#include <numeric>
+#include <bit>
+#include <functional>
+#include <utility>
+#define X first
+#define Y second
+#define INF 1e18
+#define LINF 9223372036854775807
+#define MOD 1000000007
+#define ll long long
+#define int long long
+#define pi pair<int, int>
+#define ti tuple<int, int, int>
+#define T pair<int, pi>
+#define pdi pair<double, int>
+using namespace std;
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    
+    map<int, string> M;
+    for (int i = 0; i < n; i++) {
+        string a;
+        int b;
+        cin >> a >> b;
+        M[b] = a;
+    }
+    cout << M[2026] << '\n';
+}
