db96e6b5a4456fe46ac8e4656cda9b809493ff9f
최연우 <jjangguzi@naver.com>
2025-12-23 03:37:09 +0900

    [Gold V] Title: 경숲길 재개발 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/34711. 경숲길 재개발 1/경숲길 재개발 1.cc
+++ 백준/Gold/34711. 경숲길 재개발 1/경숲길 재개발 1.cc
@@ -1,40 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <string>
-#include <deque>
 #include <math.h>
-#include <memory.h>
-#include <stack>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <tuple>
-#include <numeric>
-#include <bit>
-#include <functional>
-#include <utility>
-#define X first
-#define Y second
-#define INF 1e18
-#define LINF 9223372036854775807
-#define MOD 1000000007
-#define ll long long
 #define int long long
-#define pi pair<int, int>
-#define ti tuple<int, int, int>
-#define T pair<int, pi>
-#define pdi pair<double, int>
 using namespace std;
 int solve(int n) {
     if (n == 0) return 0;
     return solve(floor(n / 2)) + n;
 }
 signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
     int n;
     cin >> n;
     cout << solve(n);
