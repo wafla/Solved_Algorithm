751e3cf59ab7ae8b248ac243ac8aff0f8324e136
최연우 <jjangguzi@naver.com>
2025-12-22 13:45:30 +0900

    [Bronze IV] Title: 오름차순과 비내림차순, Time: 180 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34935. 오름차순과 비내림차순/오름차순과 비내림차순.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <tuple>
+#include <numeric>
+#include <bit>
+#include <functional>
+#include <utility>
+#define X first
+#define Y second
+#define INF 1e18
+#define LINF 9223372036854775807
+#define MOD 1000000007
+#define ll long long
+#define int long long
+#define pi pair<int, int>
+#define ti tuple<int, int, int>
+#define T pair<int, pi>
+#define pdi pair<double, int>
+using namespace std;
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int prev = -INF, now;
+    bool flag = 1;
+    for (int i = 0; i < n; i++) {
+        cin >> now;
+        if(prev >= now) {
+            flag = 0;
+        }
+        prev = now;
+    }
+    cout << (flag) ? 1 : 0 << '\n';
+}
